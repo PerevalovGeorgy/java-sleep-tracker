ba9b2a45f1385d5eef63fe923c8c15dc54758283
Goggy <bigggrinz@gmail.com>
2026-02-24 12:24:32 +0500

    исправления после ревью

--- src/test/practicum/TypeAnalyzerTest.java
+++ src/test/practicum/TypeAnalyzerTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TypeAnalyzerCompleteTest {
+class TypeAnalyzerTest {
 
     private ru.yandex.practicum.sleeptracker.TypeAnalyzer typeAnalyzer;
 
